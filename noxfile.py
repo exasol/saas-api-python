c8065498987e6405c384cedafb0169d0be4831f0
ckunki <christoph.kuhnke@exasol.com>
2024-04-16 15:56:41 +0200

    Added noxfile and config file for openapi-python-client

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,25 @@
 """ noxfile """
 import nox
+from nox import Session
+from noxconfig import PROJECT_CONFIG
 
 # imports all nox task provided by the toolbox
 from exasol.toolbox.nox.tasks import check # pylint: disable=unused-import
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["fix"]
+
+@nox.session(name="generate-api", python=False)
+def generate_api(session: Session):
+    """
+    Call openapi-python-client to generate the client api based on the
+    openapi specification for Exasol SaaS in JSON format, see
+    https://github.com/openapi-generators/openapi-python-client.
+    """
+    session.run(
+        "openapi-python-client",
+        "update",
+        "--url", "https://cloud.exasol.com/openapi.json",
+        "--config", "openapi_config.yml",
+    )
+    session.run("isort", "-q", "exasol/saas/client/openapi")
