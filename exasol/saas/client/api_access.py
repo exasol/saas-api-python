3867a8c5671a5e76493ca09731e0dd3a831a0a61
ckunki <christoph.kuhnke@exasol.com>
2025-02-27 10:46:36 +0100

    Added comment about additional potential breaking change

--- exasol/saas/client/api_access.py
+++ exasol/saas/client/api_access.py
@@ -186,6 +186,8 @@ class OpenApiAccess:
             region: str = "eu-central-1",
             idle_time: timedelta | None = None
     ) -> Optional[openapi.models.exasol_database.ExasolDatabase]:
+        # Renaming class database.Database to exasol_database.ExasolDatabase
+        # potentially is a breaking change.
         def minutes(x: timedelta) -> int:
             return x.seconds // 60
 
@@ -274,7 +276,7 @@ class OpenApiAccess:
             self,
             database_id: str,
     ) -> Optional[openapi.models.exasol_database.ExasolDatabase]:
-        # Replacing class database.Database by exasol_database.ExasolDatabase
+        # Renaming class database.Database to exasol_database.ExasolDatabase
         # potentially is a breaking change.
         return get_database.sync(
             self._account_id,
