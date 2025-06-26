7c5d8eb8cb9f5c03656f469924559003d079675b
Ariel Schulz <ariel.schulz@exasol.com>
2025-06-26 10:03:00 +0200

    Add sonar project and values to pyproject.toml

--- noxconfig.py
+++ noxconfig.py
@@ -9,6 +9,7 @@ ROOT_DIR = Path(__file__).parent
 class Config:
     root: Path = ROOT_DIR
     doc: Path = ROOT_DIR / "doc"
+    source: Path = Path("exasol/saas")
     version_file: Path = ROOT_DIR / "version.py"
     path_filters: Iterable[str] = ("dist", ".eggs", "venv")
 
